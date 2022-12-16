5b8b1cc185a86c6d3df6befb48e8b132f55c25b9
Tore Frederiksen <jot2re@gmail.com>
2022-12-16 14:52:35 +0100

    test: added tests and found edge cases that needed handeling

--- parsing/padding.go
+++ parsing/padding.go
@@ -12,25 +12,19 @@ func Pad(unpaddedData []byte) ([]types.FP32, error) {
 		return nil, errors.New("empty input")
 	}
 	// Compute amount of FP32 elements in the result
-	amountOfFP32s := int(math.Ceil(float64(len(unpaddedData)*8) / float64(254)))
+	amountOfFP32s := int(math.Ceil(float64(len(unpaddedData)*8) / float64(types.BitsUsedInFP32)))
 	paddedData := make([]types.FP32, amountOfFP32s, amountOfFP32s)
 	currentPadBitIdx := 0
 	for i := 0; i < amountOfFP32s; i++ {
 		currentUnpaddedSlice := getNextUnpaddedSlice(currentPadBitIdx, unpaddedData)
-		var paddedBytes [4]byte
-		shift := currentPadBitIdx % 8
-		for j := 0; j < 4; j++ {
-			paddedBytes[j] = currentUnpaddedSlice[j] << shift
-			paddedBytes[j] ^= currentUnpaddedSlice[j+1] >> (8 - shift)
-		}
-		paddedData[i] = types.FP32{Data: paddedBytes}
+		paddedData[i] = types.FP32{Data: retrieveNextFP32Byte(currentPadBitIdx, currentUnpaddedSlice)}
 		// Update currentPadBitIdx to the byte nwe need to start at which is 254 in
 		currentPadBitIdx += types.BitsUsedInFP32
 	}
 	return paddedData, nil
 }
 
-// Return a slice containing the next segment of unpadded data
+// Return a slice containing the next segment of unpadded data (without copying data), it will be a slice of either 4 or 5 bytes
 func getNextUnpaddedSlice(currentPadBitIdx int, unpaddedData []byte) []byte {
 	var upperIdx int
 	// Find the largest byte we can access in the unpadded array, in case we are in the end of the array
@@ -40,4 +34,22 @@ func getNextUnpaddedSlice(currentPadBitIdx int, unpaddedData []byte) []byte {
 		upperIdx = len(unpaddedData)
 	}
 	return unpaddedData[currentPadBitIdx/8 : upperIdx]
+}
+
+// Takes an arbitrary bit index, currentPadBitIdx and a slice of sufficient unpadded bytes to construct an FP32
+// Computes the bit offset from currentPadBitIdx and extracts the 254 bits currentUnpaddedSlice and return these in a 4 byte list
+func retrieveNextFP32Byte(currentPadBitIdx int, currentUnpaddedSlice []byte) [4]byte {
+	var paddedBytes [4]byte
+	shift := currentPadBitIdx % 8
+	for j := 0; j < 4; j++ {
+		// Check if the next bytes are there
+		if j < len(currentUnpaddedSlice) {
+			paddedBytes[j] = currentUnpaddedSlice[j] >> shift
+		}
+		// XOR in the bits from the next byte if needed (if it is there)
+		if j+1 < len(currentUnpaddedSlice) {
+			paddedBytes[j] ^= currentUnpaddedSlice[j+1] << (8 - shift)
+		}
+	}
+	return paddedBytes
 }
--- parsing/padding_test.go
+++ parsing/padding_test.go
@@ -8,19 +8,25 @@ import (
 func TestNextUnpaddedSliceSunshine(t *testing.T) {
 	unpaddedData := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 	nextSlice := getNextUnpaddedSlice(0, unpaddedData)
-	assert.Equal(t, []byte{0, 1, 2, 3, 4, 5}, nextSlice)
+	assert.Equal(t, []byte{0, 1, 2, 3, 4}, nextSlice)
+}
+
+func TestNextUnpaddedSliceSunshine2(t *testing.T) {
+	unpaddedData := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	nextSlice := getNextUnpaddedSlice(8, unpaddedData)
+	assert.Equal(t, []byte{1, 2, 3, 4, 5}, nextSlice)
 }
 
 func TestNextUnpaddedSliceMiddleOffset(t *testing.T) {
 	unpaddedData := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	// 30/8 = 3.75, since we start
+	// 30/8 = 3.75
 	nextSlice := getNextUnpaddedSlice(30, unpaddedData)
 	assert.Equal(t, []byte{3, 4, 5, 6, 7}, nextSlice)
 }
 
 func TestNextUnpaddedSliceOverflow(t *testing.T) {
 	unpaddedData := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	// 35/8 = 8.125, since we start
+	// 35/8 = 8.125
 	nextSlice := getNextUnpaddedSlice(65, unpaddedData)
 	assert.Equal(t, []byte{8, 9}, nextSlice)
 }
@@ -29,4 +35,29 @@ func TestNextUnpaddedSliceEmpty(t *testing.T) {
 	unpaddedData := []byte{}
 	nextSlice := getNextUnpaddedSlice(0, unpaddedData)
 	assert.Equal(t, []byte{}, nextSlice)
+}
+
+func TestRetrieveNextFP32ByteSunshine(t *testing.T) {
+	nextSlice := []byte{0, 1, 2, 3}
+	retrievedBytes := retrieveNextFP32Byte(0, nextSlice)
+	assert.Equal(t, [4]byte{0, 1, 2, 3}, retrievedBytes)
+}
+
+func TestRetrieveNextFP32ByteMiddle(t *testing.T) {
+	nextSlice := []byte{0b11111100, 0b11111111, 0b11111111, 0b11111111, 0b00000011}
+	retrievedBytes := retrieveNextFP32Byte(2, nextSlice)
+	assert.Equal(t, [4]byte{0b11111111, 0b11111111, 0b11111111, 0b11111111}, retrievedBytes)
+}
+
+func TestRetrieveNextFP32ByteMiddle2(t *testing.T) {
+	nextSlice := []byte{0b11111100, 0b11111111, 0b11111011, 0b11111111, 0b00000011}
+	// Add a random product of 8 to ensure that overflow gets found correctly
+	retrievedBytes := retrieveNextFP32Byte(8*100+3, nextSlice)
+	assert.Equal(t, [4]byte{0b11111111, 0b01111111, 0b11111111, 0b01111111}, retrievedBytes)
+}
+
+func TestRetrieveNextFP32ByteOverflow(t *testing.T) {
+	nextSlice := []byte{0b11111111, 0b10000000}
+	retrievedBytes := retrieveNextFP32Byte(4, nextSlice)
+	assert.Equal(t, [4]byte{0b00001111, 0b00001000, 0b00000000, 0b00000000}, retrievedBytes)
 }
