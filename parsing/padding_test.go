9647d8201a4345cb16090881a18e9d97c981de04
Tore Frederiksen <jot2re@gmail.com>
2022-12-19 14:38:46 +0100

    fix: updated and fixed tests

--- parsing/padding_test.go
+++ parsing/padding_test.go
@@ -36,33 +36,33 @@ func TestPadSunshine(t *testing.T) {
 
 // PRIVATE METHOD TESTS
 func TestNextUnpaddedSliceSunshine(t *testing.T) {
-	unpaddedData := make([]byte, 40)
-	for i := range unpaddedData {
-		unpaddedData[i] = byte(i)
-	}
+	unpaddedData := getMonotoneTestData(40)
 	nextSlice := getNextUnpaddedSlice(0, unpaddedData)
 	expected := unpaddedData[:33]
 	assert.Equal(t, expected, nextSlice)
 }
 
 func TestNextUnpaddedSliceSunshine2(t *testing.T) {
-	unpaddedData := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	unpaddedData := getMonotoneTestData(40)
 	nextSlice := getNextUnpaddedSlice(8, unpaddedData)
-	assert.Equal(t, []byte{1, 2, 3, 4, 5}, nextSlice)
+	expected := unpaddedData[1:34]
+	assert.Equal(t, expected, nextSlice)
 }
 
 func TestNextUnpaddedSliceMiddleOffset(t *testing.T) {
-	unpaddedData := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	unpaddedData := getMonotoneTestData(40)
 	// 30/8 = 3.75
 	nextSlice := getNextUnpaddedSlice(30, unpaddedData)
-	assert.Equal(t, []byte{3, 4, 5, 6, 7}, nextSlice)
+	expected := unpaddedData[3:36]
+	assert.Equal(t, expected, nextSlice)
 }
 
 func TestNextUnpaddedSliceOverflow(t *testing.T) {
-	unpaddedData := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	// 35/8 = 8.125
+	unpaddedData := getMonotoneTestData(40)
+	// 65/8 = 8.125
 	nextSlice := getNextUnpaddedSlice(65, unpaddedData)
-	assert.Equal(t, []byte{8, 9}, nextSlice)
+	expected := unpaddedData[8:40]
+	assert.Equal(t, expected, nextSlice)
 }
 
 func TestNextUnpaddedSliceEmpty(t *testing.T) {
@@ -72,13 +72,11 @@ func TestNextUnpaddedSliceEmpty(t *testing.T) {
 }
 
 func TestRetrieveNextFP32ByteSunshine(t *testing.T) {
-	nextSlice := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31}
+	nextSlice := getMonotoneTestData(32)
 
 	retrievedBytes := retrieveNextFP32Byte(0, nextSlice)
 
-	var expected [types.BytesUsedInFP32]byte
-	copy(expected[:], nextSlice[:types.BytesUsedInFP32])
-	assert.Equal(t, expected, retrievedBytes)
+	assert.Equal(t, []byte(nextSlice), retrievedBytes[:])
 }
 
 func TestRetrieveNextFP32ByteMiddle(t *testing.T) {
@@ -93,38 +91,35 @@ func TestRetrieveNextFP32ByteMiddle(t *testing.T) {
 
 	expected := make([]byte, types.BytesUsedInFP32)
 	set1s(&expected, 0, types.BytesUsedInFP32)
-	var expectedFixed [types.BytesUsedInFP32]byte
-	copy(expectedFixed[:], expected[:types.BytesUsedInFP32])
-	assert.Equal(t, expectedFixed, retrievedBytes)
+	expected[31] &= 0b00111111
+	assert.Equal(t, expected, retrievedBytes[:])
 }
 
 func TestRetrieveNextFP32ByteMiddle2(t *testing.T) {
 	nextSlice := make([]byte, types.BytesUsedInFP32+1)
 	// Set 0 at the edge to test edge case
-	nextSlice[0] = 0b11111100
+	nextSlice[0] = 0b11111000
 	set1s(&nextSlice, 1, types.BytesUsedInFP32)
 	// Set an arbitrary middle bit to 0
 	nextSlice[5] = 0b11111011
 	// Set 0 at the edge to test edge case
-	nextSlice[types.BytesUsedInFP32] = 0b00000011
+	nextSlice[types.BytesUsedInFP32] = 0b11111110
 
-	// Add a arbitrary product of 8 to ensure that overflow gets found correctly
+	// Add an arbitrary product of 8 to ensure that overflow gets found correctly
 	retrievedBytes := retrieveNextFP32Byte(8*100+3, nextSlice)
 
 	expected := make([]byte, types.BytesUsedInFP32)
 	set1s(&expected, 0, types.BytesUsedInFP32)
 	// Check that the 0 bits are in the expected places
 	expected[4] = 0b01111111
-	expected[types.BytesUsedInFP32-1] = 0b01111111
-	var expectedFixed [types.BytesUsedInFP32]byte
-	copy(expectedFixed[:], expected[:types.BytesUsedInFP32])
-	assert.Equal(t, expectedFixed, retrievedBytes)
+	expected[types.BytesUsedInFP32-1] = 0b00011111
+	assert.Equal(t, expected, retrievedBytes[:])
 }
 
 func TestRetrieveNextFP32ByteOverflow(t *testing.T) {
 	nextSlice := []byte{0b11111111, 0b10000000}
 
-	// Add a arbitrary product of 8 to ensure that overflow gets found correctly
+	// Add an arbitrary product of 8 to ensure that overflow gets found correctly
 	retrievedBytes := retrieveNextFP32Byte(8*1000+4, nextSlice)
 
 	var expected [types.BytesUsedInFP32]byte
@@ -133,8 +128,20 @@ func TestRetrieveNextFP32ByteOverflow(t *testing.T) {
 	assert.Equal(t, expected, retrievedBytes)
 }
 
+/**
+ *	HELPER FUNCTIONS
+ */
+
 func set1s[ARRAY types.FP32Array](input *ARRAY, startInclude int, stopExclusive int) {
 	for i := startInclude; i < stopExclusive; i++ {
 		(*input)[i] = 0b11111111
 	}
+}
+
+func getMonotoneTestData(amount int) []byte {
+	unpaddedData := make([]byte, amount)
+	for i := range unpaddedData {
+		unpaddedData[i] = byte(i)
+	}
+	return unpaddedData
 }
