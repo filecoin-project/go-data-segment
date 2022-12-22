70b899685a48b04092dfaeb63fd87b23b200c101
Tore Frederiksen <jot2re@gmail.com>
2022-12-22 14:30:25 +0100

    feat: added subtree proof method

--- merkleTree/merkleTree_test.go
+++ merkleTree/merkleTree_test.go
@@ -82,29 +82,86 @@ func TestConstructProof(t *testing.T) {
 	tree, _ := getTree(t, 130)
 
 	// Construct a proof of a leaf node
-	proof := tree.ConstructProof(tree.Depth()-1, 55)
+	proof, err := tree.ConstructProof(tree.Depth()-1, 55)
+	assert.Nil(t, err)
 
 	assert.Equal(t, proof.lvl, log2Ceil(tree.Leafs()))
 	assert.Equal(t, proof.idx, 55)
 	assert.Equal(t, len(proof.path), tree.Depth()-1)
 }
 
-func TestValidateProofSunshine(t *testing.T) {
-	tree, leaf := getTree(t, 130)
-	// Construct a proof of a leaf node
-	proof := tree.ConstructProof(tree.Depth()-1, 55)
-
-	assert.True(t, proof.Validate(leaf, tree))
+func TestValidateLeafSunshine(t *testing.T) {
+	testAmounts := []int{130, 255, 256, 257, 1000000}
+	for _, amount := range testAmounts {
+		tree, leaf := getTree(t, amount)
+		// Construct a proof of a leaf node
+		proof, err := tree.ConstructProof(tree.Depth()-1, 0)
+		assert.Nil(t, err)
+		assert.True(t, proof.ValidateLeaf(leaf, tree.GetRoot()))
+		proof, err = tree.ConstructProof(tree.Depth()-1, amount-1)
+		assert.Nil(t, err)
+		assert.True(t, proof.ValidateLeaf(leaf, tree.GetRoot()))
+		proof, err = tree.ConstructProof(tree.Depth()-1, amount/2-5)
+		assert.Nil(t, err)
+		assert.True(t, proof.ValidateLeaf(leaf, tree.GetRoot()))
+	}
 }
 
-func TestValidateProofNegative(t *testing.T) {
-	tree, leaf := getTree(t, 266)
-	// Construct a proof of a leaf node
-	proof := tree.ConstructProof(tree.Depth()-1, 4)
-	// Corrupt a bit in a node
-	proof.path[3].data[0] ^= 1
+func TestValidateLeafNegative(t *testing.T) {
+	testAmounts := []int{68, 511, 512, 513, 1000000}
+	for _, amount := range testAmounts {
+		tree, leaf := getTree(t, amount)
+		// Construct a proof of a leaf node
+		proof, err := tree.ConstructProof(tree.Depth()-1, 4)
+		assert.Nil(t, err)
+		for currentLvl := 0; currentLvl < tree.Depth()-1; currentLvl++ {
+			for i := 0; i < digestBytes; i++ {
+				// Corrupt a bit in a node
+				// Note that modifying the most significant bits of the last byte will still result in failure even tough those bits should never be set
+				proof.path[currentLvl].data[i] ^= 0b10000000
+				assert.False(t, proof.ValidateLeaf(leaf, tree.GetRoot()))
+			}
+		}
+	}
+}
 
-	assert.False(t, proof.Validate(leaf, tree))
+func TestValidateProofSubtree(t *testing.T) {
+	testAmounts := []int{1300, 65535, 65536, 65537}
+	for _, amount := range testAmounts {
+		tree, _ := getTree(t, amount)
+		for lvl := 1; lvl < tree.Depth(); lvl++ {
+			// Test the smallest node in the level
+			proof, err := tree.ConstructProof(lvl, 0)
+			assert.Nil(t, err)
+			assert.True(t, proof.ValidateSubtree(&tree.nodes[lvl][0], tree.GetRoot()))
+
+			// Test the largest node in the level
+			proof, err = tree.ConstructProof(lvl, len(tree.nodes[lvl])-1)
+			assert.Nil(t, err)
+			assert.True(t, proof.ValidateSubtree(&tree.nodes[lvl][len(tree.nodes[lvl])-1], tree.GetRoot()))
+
+			// Test a node in the middle of the level
+			proof, err = tree.ConstructProof(lvl, len(tree.nodes[lvl])/3)
+			assert.Nil(t, err)
+			assert.True(t, proof.ValidateSubtree(&tree.nodes[lvl][len(tree.nodes[lvl])/3], tree.GetRoot()))
+		}
+	}
+}
+
+func TestValidateSubtreeNegative(t *testing.T) {
+	testAmounts := []int{68, 511, 512, 513, 1000000}
+	for _, amount := range testAmounts {
+		tree, _ := getTree(t, amount)
+		for currentLvl := 1; currentLvl < tree.Depth()-1; currentLvl++ {
+			// Construct a proof of the second to most right node
+			idx := len(tree.nodes[currentLvl]) - 2
+			proof, err := tree.ConstructProof(currentLvl, idx)
+			assert.Nil(t, err)
+			// Corrupt a bit in a node
+			proof.path[currentLvl/3].data[0] ^= 0b10000000
+			assert.False(t, proof.ValidateSubtree(&tree.nodes[currentLvl][idx], tree.GetRoot()))
+		}
+	}
 }
 
 // PRIVATE METHOD TESTS
